a4015245d416bbc6187d8ff0fb0415b5726572be
behnamandarz <53329662+behnamandarz@users.noreply.github.com>
2020-09-07 07:07:19 +0430

    Create MainActivity.java

---
+++ native_thread_global_var/src/main/java/org/arguslab/native_thread_global_var/MainActivity.java
@@ -0,0 +1,49 @@
+package org.arguslab.native_global_var;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.telephony.TelephonyManager;
+
+/**
+ * @description The value v of a source is sent to native lib via jni and leaked by global variables via threads.
+ * @challenges The analysis must be able to track data flow in both java and native layers to capture the data leakage considering native threads.
+ */
+public class MainActivity extends Activity {
+
+    static {
+        System.loadLibrary("leak"); // "libleak.so"
+    }
+
+    public static native void send(String data);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, 1);
+        }
+    }
+
+    private void leakImei() {
+        TelephonyManager tel = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        String imei = tel.getDeviceId(); // source
+        send(imei);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                leakImei();
+                return;
+            }
+        }
+    }
+}
