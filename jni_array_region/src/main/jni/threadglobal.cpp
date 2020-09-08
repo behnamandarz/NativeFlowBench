30e27cacbb5fdc570643bc0d33970e8a827aca8f
bandarz <53329662+behnamandarz@users.noreply.github.com>
2020-09-09 02:09:57 +0430

    Create threadglobal.cpp

---
+++ jni_array_region/src/main/jni/threadglobal.cpp
@@ -0,0 +1,57 @@
+#include <android/log.h>
+#include <jni.h>
+#include <stddef.h>
+
+
+#define LOG_TAG    "leak"
+#define LOGI(...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+extern "C" {
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1leak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data);
+
+}
+
+const char *getCharFromString(JNIEnv *env, jstring string) {
+    if (string == NULL)
+        return NULL;
+
+    return env->GetStringUTFChars(string, 0);
+}
+int retStatus;
+void *run_by_thread(void *arg) {
+    int cnt = 3, i;
+    int* threadNum = (int*)arg;
+    for (i = 0; i < cnt; ++i) {
+    sleep(1);
+    LOGI(1, "thread %d: %d", *threadNum, i);
+    }
+    if (1 == *threadNum) {
+    retStatus = 100;
+    return (void*)&retStatus;
+    } else if (2 == *threadNum) {
+    retStatus = 200;
+    pthread_exit((void*)&retStatus);
+    }
+}
+JNIEXPORT void JNICALL
+Java_org_arguslab_native_1leak_MainActivity_send(JNIEnv *env, jobject thisObj, jstring data) {
+    pthread_t th1, th2;
+    int threadNum1 = 1, threadNum2 = 2;
+    int ret;
+    ret = pthread_create(&th1, NULL, run_by_thread,
+    (void*)&threadNum1);
+    ret = pthread_create(&th2, NULL, run_by_thread,
+    (void*)&threadNum2);
+    void *status;
+    ret = pthread_join(th1, &status);
+    int* st = (int*)status;
+    LOGI(1, "thread 1 end %d %d", ret, *st);
+    ret = pthread_join(th2, &status);
+    st = (int*)status;
+    LOGI(1, "thread 2 end %d %d", ret, *st);
+    LOGI("%s", getCharFromString(env, data)); // leak
+    return;
+}
